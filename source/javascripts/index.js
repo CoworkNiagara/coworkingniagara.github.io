43a4b5dc679040485b10c30122221f24327ff801
Trevor Twining <trevortwining@gmail.com>
2015-01-22 17:35:50 -0500

    Commenting out auto-sizing div. Will remove soon

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,12 +1,11 @@
 (function($) {
   $(document).ready(function() {
-    var resizeWelcome = function() {
-      var height = $(window).height();
-
-      $(".welcome").height(height);
-    };
-
-    resizeWelcome();
-    $(window).resize(resizeWelcome);
+    //Temporarily commenting out
+    //var resizeWelcome = function() {
+    //  var height = $(window).height();
+    //  $(".welcome").height(height);
+    //};
+    //resizeWelcome();
+    //$(window).resize(resizeWelcome);
   });
 })(jQuery);
