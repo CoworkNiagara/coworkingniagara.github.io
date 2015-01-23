04c393c13cc2ab4bac5d2f074f6c20b23c731518
Adam Daniels <adam@mediadrive.ca>
2015-01-23 17:58:36 -0500

    Initial build subtree commit

---
+++ build/javascripts/index.js
@@ -0,0 +1,11 @@
+(function($) {
+  $(document).ready(function() {
+    //Temporarily commenting out
+    //var resizeWelcome = function() {
+    //  var height = $(window).height();
+    //  $(".welcome").height(height);
+    //};
+    //resizeWelcome();
+    //$(window).resize(resizeWelcome);
+  });
+})(jQuery);
