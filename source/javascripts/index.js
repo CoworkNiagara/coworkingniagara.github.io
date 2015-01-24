dec1294a8c3466bc0504eaa6e4e637b9fbf0c1fe
Adam Daniels <adam@mediadrive.ca>
2015-01-23 22:55:53 -0500

    Mobile breakpoint

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,5 +1,9 @@
 (function($) {
   $(document).ready(function() {
+    $(".mobile-toggle").on("click", function() {
+      $(".header .navigation").toggle();
+    });
+
     //Temporarily commenting out
     //var resizeWelcome = function() {
     //  var height = $(window).height();
